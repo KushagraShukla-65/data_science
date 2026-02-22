239a60aaa0cc493904bc582bf0d8a6845a627407
KushagraShukla-65 <kushagra74890@gmail.com>
2026-02-22 18:30:39 +0530

    task 1

---
+++ variable.py
@@ -0,0 +1,6 @@
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+height=float(input("Enter your height:"))
+print("Your name is",name,"which is of datatype",type(name))
+print("Your age is",age,"which is of datatype",type(age))
+print("Your height is",height,"which is of datatype",type(height))
